7d3b1ad1a790737759cba481dfd49337427709dd
KristianWB <42993999+KristianWB@users.noreply.github.com>
2020-04-25 13:40:19 +0200

    First

---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPageMovie.Data;
+using System;
+using System.Linq;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPageMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Conan the Barbarian",
+                        ReleaseDate = DateTime.Parse("2011-1-08"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Total Recall",
+                        ReleaseDate = DateTime.Parse("1990-1-06"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "XXX files 2",
+                        ReleaseDate = DateTime.Parse("2005-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "WITT, KONTANT og MARTINI GOES TO TIMBUKTU",
+                        ReleaseDate = DateTime.Parse("2021-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
